67e264aac82ba8625998020c45d431e207d9a4ff
phamkhacan1903 <phamkhacan1903@gmail.com>
2019-02-17 23:38:59 +0700

    Lab_MVCvsFlat

---
+++ Lab_MVCvsFlat/Flat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FLat</title>
+</head>
+<body>
+	<form>
+		a = <input type="text" name="a" value=""> <br>
+		b = <input type="text" name="b" value=""> <br>
+		c = <input type="text" name="c" value=""> <br>
+		<input type="submit" value="Chấp nhận"/>
+ 	</form>
+</body>
+</html>
